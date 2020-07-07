14ebe5b0db23a516c7bb94662ca106c48322a54c
cyberbarbie <taeluralexis@gmail.com>
2020-07-07 16:36:57 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,55 @@
+let condiments = ['Ketchup', 'Mustard', 'Soy Sauce', 'Sriracha'];
+
+const utensils = ['Fork', 'Knife', 'Chopsticks', 'Spork'];
+
+condiments[0] = "Mayo"
+
+condiments = ["Mayo"]
+
+utensils[3] = "Spoon"
+
+const objectives = ['Learn a new languages', 'Read 52 books', 'Run a marathon'];
+
+// .push()- add item to end of array
+objectives.push("build online platform", "release newsletter")
+
+const chores = ['wash dishes', 'do laundry', 'take out trash'];
+
+chores.push("sleep", "sleep more")
+
+// .pop()- removes last item from array
+
+chores.pop()
+
+const groceryList = ['orange juice', 'bananas', 'coffee beans', 'brown rice', 'pasta', 'coconut oil', 'plantains'];
+
+groceryList.shift()
+
+
+// .unshift()- add to the beginning of an array
+
+groceryList.unshift("popcorn")
+
+console.log(groceryList)
+
+// .slice()- slice elements and return a new array
+
+
+
+let goalsOf2020 = ["Secure a new role", "deliver the next few conference talks", "create a revenue stream"]
+
+goalsOf2020[3] = "Learn how to garden"
+
+console.log(goalsOf2020)
+
+// .length()- returns the number of items in array
+
+console.log(goalsOf2020.length);
+
+// .push() - to add items to array
+
+goalsOf2020.push("")
+
+
+
+

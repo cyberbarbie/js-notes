ef0233951de4f754baa33d2672bc4e32a7f019b1
cyberbarbie <taeluralexis@gmail.com>
2020-07-07 18:45:38 -0400

    adde more info about arrow functions and the mutation of arrays

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ groceryList.shift()
 
 groceryList.unshift("popcorn")
 
-console.log(groceryList)
+//console.log(groceryList)
 
 // .slice()- slice elements and return a new array
 
@@ -53,8 +53,74 @@ goalsOf2020.push("Use Scarbill's name in my code", "Build full stack web app")
 // remove items from an array
 goalsOf2020.pop()
 
-console.log(goalsOf2020)
+//console.log(goalsOf2020)
 
+// .shift()- remove first item from an array
+
+goalsOf2020.shift()
+
+// .unshift()- add to the beginning of an array 
+
+goalsOf2020.unshift("Build and release full stack web app with node express sql and react")
+
+// .slice()- 
+
+//const groceryList = ['orange juice', 'bananas', 'coffee beans', 'brown rice', 'pasta', 'coconut oil', 'plantains'];
+
+// slice elemenets from an array beginning and end + 1 
+groceryList.slice(1, 4)
+
+// indexOf- return the position of an element in an array
+
+const pastaIndex = groceryList.indexOf("pasta")
+
+//console.log(pastaIndex)
+
+const goals = ["Build app", "stream every day", "release monthly newsletter"]
+
+function newGoal(arr) {
+    arr.push("Learn something new")
+}
+
+newGoal(goals)
+
+//console.log(goals)
+
+// if the arrow func is only returning one statement, you dont have to explicitly state the kw
+// one parameter makes the parenthesis optional
+
+const greeting = user => `Hello have a good day ${user}`
+
+//console.log(greeting("Tae'lur"))
+
+
+
+/*
+
+Pass by reference- what you pass the functi0on is a reference to where the variable
+memory is stored and changing the memory
+ex. within a function you change the values of an array defined outside of it and
+in the process it permanently changes its value
+
+*/
+
+const concept = ['arrays', 'can', 'be', 'mutated'];
+
+// creates a function that modifies an existing element in an array
+function changeArr(arr){
+  arr[3] = 'MUTATED';
+}
+
+changeArr(concept);
+
+// creates a function that deletes the last element of whatever array is passed in as an argument
+function removeElement(newArr) {
+    newArr.pop()
+}
+
+removeElement(concept)
+
+console.log(concept)
 
 
 
